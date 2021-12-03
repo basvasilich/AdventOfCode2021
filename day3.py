adc0ade56cb40e4c7e466d5de7a575a94005eb8d
Vadim Patsev <basvasilich@yandex-team.ru>
2021-12-03 23:28:51 +0300

    day 3 part 2

--- day3.py
+++ day3.py
@@ -34,8 +34,49 @@ def solution_part1(filename):
     return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
+def solution_part2(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+
+    if len(lines) == 0:
+        return 0
+
+    def helper(lines_list: list[str], index: int, oxy: bool) -> str:
+        if len(lines_list) == 1:
+            return lines_list[0]
+
+        list_0 = []
+        list_1 = []
+        for line in lines_list:
+            line_bites = list(line)
+            bite = int(line_bites[index])
+            if bite:
+                list_1.append(line)
+            else:
+                list_0.append(line)
+
+        if len(list_1) > len(list_0):
+            if oxy:
+                return helper(list_1, index + 1, oxy)
+            else:
+                return helper(list_0, index + 1, oxy)
+        elif len(list_1) == len(list_0):
+            if oxy:
+                return helper(list_1, index + 1, oxy)
+            else:
+                return helper(list_0, index + 1, oxy)
+        else:
+            if oxy:
+                return helper(list_0, index + 1, oxy)
+            else:
+                return helper(list_1, index + 1, oxy)
+
+    return int(helper(lines, 0, True), 2) * int(helper(lines, 0, False), 2)
+
+
 assert (solution_part1('input/day3.test.txt') == 198)
 print('Result Part 1: ', solution_part1('input/day3.txt'))
 
-# assert (solution_part2('input/day2.test.txt') == 900)
-# print('Result Part 2: ', solution_part2('input/day2.txt'))
+assert (solution_part2('input/day3.test.txt') == 230)
+print('Result Part 2: ', solution_part2('input/day3.txt'))
