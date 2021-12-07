586235dc014623fe975c18600c599d8c155c0676
Vadim Patsev <basvasilich@yandex-team.ru>
2021-12-07 23:38:23 +0300

    day 7

--- day7.py
+++ day7.py
@@ -6,28 +6,58 @@ def read_input(filename) -> list[(str, int)]:
         return [int(x) for x in line[0].rstrip("\n").split(',')]
 
 
-def solution(filename, day) -> int:
-    input_vals = read_input(filename)
+def solution(filename: str, part2: bool = False) -> int:
+    vals = read_input(filename)
 
-    for val in input_vals:
-        store[val] += 1
+    memo = {}
 
-    while day:
-        tmp = [0] * 9
-        val_0 = store[0]
+    def calculate_fuel(vals, point, part2):
+        if point in memo:
+            return memo[point]
 
-        for i in range(1, 9):
-            tmp[i - 1] = store[i]
-        tmp[8] += val_0
-        tmp[6] += val_0
-        store = tmp
-        day -= 1
+        result = 0
+        if part2:
+            for val in vals:
+                n = abs(point - val)
+                result += (pow(n, 2) + n) / 2
+        else:
+            for val in vals:
+                result += abs(point - val)
 
-    return sum(store)
+        memo[point] = result
+        return result
+
+    def helper(s, e, _min_fuel, part2):
+        if e < s:
+            return _min_fuel
+
+        if e == s:
+            return min(_min_fuel, calculate_fuel(vals, s, part2))
+
+        mid = s + (e - s) // 2
+
+        s_fuel = calculate_fuel(vals, s, part2)
+        e_fuel = calculate_fuel(vals, e, part2)
+        mid_fuel = calculate_fuel(vals, mid, part2)
+        _min_fuel = min(s_fuel, e_fuel, mid_fuel, _min_fuel)
+
+        if abs(s_fuel - mid_fuel) > abs(e_fuel - mid_fuel):
+            return helper(mid + 1, e, _min_fuel, part2)
+        else:
+            return helper(s, mid, _min_fuel, part2)
+
+    max_val = max(vals)
+
+    min_fuel = max_val * len(vals)
+
+    if part2:
+        min_fuel = (max_val * (max_val + 1) * len(vals)) / 2
+
+    return helper(min(vals), max_val, min_fuel, part2)
 
 
-assert (solution('input/day6.test.txt', 80) == 5934)
-print('Result Part 1: ', solution('input/day6.txt', 80))
+assert (solution('input/day7.test.txt') == 37)
+print('Result Part 1: ', solution('input/day7.txt'))
 
-assert (solution('input/day6.test.txt', 256) == 26984457539)
-print('Result Part 1: ', solution('input/day6.txt', 256))
+assert (solution('input/day7.test.txt', True) == 168)
+print('Result Part 2: ', solution('input/day7.txt', True))
