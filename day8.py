346862cacc573d517d98e398e95022afa275a120
Vadim Patsev <basvasilich@yandex.ru>
2021-12-09 01:48:05 +0300

    day 8

--- day8.py
+++ day8.py
@@ -16,13 +16,69 @@ def read_input(filename) -> (list[(list[str], list[str])], int):
         return vals, result_part1
 
 
+def string_diff(a: str, b: str) -> str:
+    a_set = set(a)
+    b_set = set(b)
+    if len(a_set) > len(b_set):
+        r = "".join(b_set - a_set)
+        return r
+    else:
+        r = "".join(a_set - b_set)
+        return r
+
+
 def solution_part1(filename: str, ) -> int:
     _, result_part1 = read_input(filename)
     return result_part1
 
 
+def solution_part2(filename: str, ) -> int:
+    vals, _ = read_input(filename)
+
+    result = 0
+
+    for inp_list, out_list in vals:
+        digits = {}
+        inp_list = ["".join(sorted(s)) for s in inp_list]
+        for s in inp_list:
+            if len(s) == 2:
+                digits[1] = s
+            elif len(s) == 3:
+                digits[7] = s
+            elif len(s) == 7:
+                digits[8] = s
+            elif len(s) == 4:
+                digits[4] = s
+
+        for s in inp_list:
+            if len(s) == 6 and len(string_diff(s, digits[7])) == 1:
+                digits[6] = s
+            elif len(s) == 5 and len(string_diff(s, digits[1])) == 0:
+                digits[3] = s
+
+        c_char = string_diff(digits[6], digits[7])
+
+        for s in inp_list:
+            if len(s) == 6 and s not in digits.values() and len(string_diff(s, digits[4])) == 0:
+                digits[9] = s
+            elif len(s) == 5 and s not in digits.values() and c_char not in s:
+                digits[5] = s
+
+        for s in inp_list:
+            if len(s) == 6 and s not in digits.values():
+                digits[0] = s
+            elif len(s) == 5 and s not in digits.values():
+                digits[2] = s
+
+        chars = {v: str(k) for k, v in digits.items()}
+        result_list = [chars["".join(sorted(s))] for s in out_list]
+        result_str = "".join(result_list)
+        result += int(result_str)
+    return result
+
+
 assert (solution_part1('input/day8.test.txt') == 26)
 print('Result Part 1: ', solution_part1('input/day8.txt'))
-#
-# assert (solution('input/day7.test.txt', True) == 168)
-# print('Result Part 2: ', solution('input/day7.txt', True))
+
+assert (solution_part2('input/day8.test.txt') == 61229)
+print('Result Part 2: ', solution_part2('input/day8.txt'))
